992d3ec19f999184d97d1d41191b1cfe0396fe89
alby <alby@foxmail.com>
2017-10-31 23:46:04 +0800

    XLMenu: 定义递归组件

---
+++ src/components/XLMenu.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-menu-item v-if="model.type === 0" :index="model.index">
+    <i class="el-icon-menu"></i>
+    <span slot="title">{{ model.title }}</span>
+  </el-menu-item>
+  <el-submenu v-else-if="model.type === 1" :index="model.index">
+    <template slot="title">
+      <i class="el-icon-menu"></i>
+      <span slot="title">{{ model.title }}</span>
+    </template>
+    <xl-menu v-for="(item, itemindex) in model.children" :key="model.index + '-' + itemindex" :model="item" :index="model.index + '-' + itemindex"></xl-menu>
+  </el-submenu>
+  <el-submenu v-else-if="model.type === 2" :index="model.index">
+    <template slot="title">
+      <i class="el-icon-menu"></i>
+      <span slot="title">{{ model.title }}</span>
+    </template>
+    <xl-menu v-for="(item, itemindex) in model.children" :key="model.index + '-' + itemindex" :model="item" :index="model.index + '-' + itemindex"></xl-menu>
+  </el-submenu>
+</template>
+
+<script>
+// model.type: 为 1 或 2 时，model.children 不包含元素属于异常情况
+// index: 目的是传递给子节点用于构造 key 和 el 组件的 index
+export default {
+  name: 'XLMenu',
+  props: ['model', 'index'],
+  data () {
+    return {
+    }
+  }
+}
+</script>
--- src/modules/index/index.js
+++ src/modules/index/index.js
@@ -3,12 +3,15 @@ import ElementUI from 'element-ui'
 import '../../theme/element-#2D2F33/index.css'
 import App from './App.vue'
 import HttpClient from '@/utils/httpclient.js'
+import XLMenu from '@/components/XLMenu.vue'
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
 
 Vue.use(HttpClient)
+
+Vue.component('xl-menu', XLMenu)
 
 /* eslint-disable no-new */
 new Vue({
