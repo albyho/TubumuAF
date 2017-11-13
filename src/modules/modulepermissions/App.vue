611f8c51ae3f417abcedfe7dbe065db82ed706f4
alby <alby@foxmail.com>
2017-11-13 18:23:56 +0800

    modulepermissions 的 Api 实现

---
+++ src/modules/modulepermissions/App.vue
@@ -0,0 +1,82 @@
+<template>
+<el-container>
+  <el-header class="header">  
+    <el-breadcrumb separator-class="el-icon-arrow-right" class="breadcrumb">
+      <el-breadcrumb-item>模块管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+  </el-header>
+  <el-main class="main">      
+    <el-row>  
+      <el-input placeholder="输入关键字进行过滤" size="small" v-model="filterText"></el-input>  
+    </el-row>
+    <el-row>
+      <el-tree
+        :data="treeData" 
+        :props="treeDefaultProps" 
+        :empty-text="emptyText"
+        node-key="id" 
+        ref="tree"
+        :filter-node-method="filterNode"
+        default-expand-all>
+      </el-tree>
+    </el-row>
+  </el-main>
+</el-container>
+</template>
+
+<script>
+import api from '@/utils/api'
+
+export default {
+  data () {
+    return {
+      isLoading: false,
+      treeData: null,
+      treeDefaultProps: {
+        children: 'children',
+        label: 'name'
+      },
+      filterText: null
+    }
+  },
+  mounted () {
+    // this.getTree()
+  },
+  computed: {
+    emptyText: function () {
+      return this.isLoading ? '加载中...' : '暂无数据'
+    }
+  },
+  watch: {
+    filterText (val) {
+      this.$refs.tree.filter(val)
+    }
+  },
+  methods: {
+    getTree () {
+      this.isLoading = true
+      api.getPermissionTree().then(response => {
+        this.isLoading = false
+        this.treeData = response.data.tree
+      }, error => {
+        this.isLoading = false
+        this.$message({
+          message: error.message,
+          type: 'error'
+        })
+      })
+    },
+    filterNode (value, data) {
+      if (!value) {
+        return true
+      }
+      return data.name.indexOf(value) !== -1
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/modules/modulepermissions/modulepermissions.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import ElementUI from 'element-ui'
+// import { Form, FormItem, Button } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import '../../styles/inner.scss'
+import App from './App.vue'
+import HttpClient from '@/utils/httpclient.js'
+
+Vue.config.productionTip = false
+
+// 全部引入
+Vue.use(ElementUI)
+// 按需引入
+/*
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Button)
+*/
+
+Vue.use(HttpClient)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
