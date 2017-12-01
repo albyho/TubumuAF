2d8b46670c2bb49150f08a5e088d924403edd554
alby <alby@foxmail.com>
2017-12-01 14:56:16 +0800

    config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,11 @@
-// Development
+const debugAccount = 'system'
+const debugPassword = '111111'
+
 const isProduction = process.env.NODE_ENV === 'production'
 
 export const baseURL = isProduction ? '/manager/api' : 'http://127.0.0.1:9005/manager/api'
-export const defaultAccount = isProduction ? '' : 'system'
-export const defaultPassword = isProduction ? '' : '111111'
+export const defaultAccount = isProduction ? '' : debugAccount
+export const defaultPassword = isProduction ? '' : debugPassword
 
 // Development
 // export const baseURL = 'http://127.0.0.1:9005/manager/api'
