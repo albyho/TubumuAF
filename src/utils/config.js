59a0579f9fa86cb9a7d23d09efede808037ad069
alby <alby@foxmail.com>
2018-10-10 23:14:14 +0800

    Update package

--- src/utils/config.js
+++ src/utils/config.js
@@ -6,13 +6,3 @@ const isProduction = process.env.NODE_ENV === 'production'
 export const baseURL = isProduction ? '/manager/api' : 'http://127.0.0.1:9007/manager/api'
 export const defaultAccount = isProduction ? '' : debugAccount
 export const defaultPassword = isProduction ? '' : debugPassword
-
-// Development
-// export const baseURL = 'http://127.0.0.1:9005/manager/api'
-// export const defaultAccount = 'system'
-// export const defaultPassword = '111111'
-
-// Production
-// export const baseURL = '/manager/api'
-// export const defaultAccount = ''
-// export const defaultPassword = ''
